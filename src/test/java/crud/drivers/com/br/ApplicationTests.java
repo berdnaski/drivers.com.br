d7c7b58f5cc2215e629f3d7db5fba471498b3da8
berdnaski <berdnaski1177@gmail.com>
2024-04-15 00:42:13 -0300

    Initial commit

---
+++ src/test/java/crud/drivers/com/br/ApplicationTests.java
@@ -0,0 +1,13 @@
+package crud.drivers.com.br;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
